881ee0fd71f785f747e099807011bb276e7181c1
Alessandro Pinto <Alessandro.Pinto@ext.europassistance.it>
2023-10-24 21:43:27 +0200

    added atlas db

--- pages/api/glp.js
+++ pages/api/glp.js
@@ -1,11 +1,42 @@
 // pages/api/GetLastPosition.js
-import fs from "fs";
-import path from "path";
+//import { MongoClient, ObjectId } from "mongodb";
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(
+  "mongodb+srv://pedibus:Ped45fimi@cluster0.zfzxyjk.mongodb.net/pedibus?retryWrites=true&w=majority",
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
 
 const handler = async (req, res) => {
   if (req.method === "GET") {
-    const data = fs.readFileSync(path.resolve("./", "data.json"), "utf-8");
-    res.status(200).json(JSON.parse(data));
+    try {
+      if (!client.topology || !client.topology.isConnected()) {
+        await client.connect();
+      }
+      const db = client.db("pedibus"); // replace with your database name
+      const collection = db.collection("positions"); // replace with your collection name
+
+      // Convert string id to ObjectId
+      const id = "6537ff14064d2b65bd701ca4";
+
+      const document = await collection.findOne({
+        _id: id,
+      });
+      if (document) {
+        res.status(200).json(document);
+      } else {
+        res.status(404).send("Document not found");
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
   } else {
     res.status(405).send("Method Not Allowed");
   }
--- pages/api/position.js
+++ pages/api/position.js
@@ -1,14 +1,47 @@
 // pages/api/position.js
-import fs from "fs";
-import path from "path";
+//import { MongoClient } from "mongodb";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(
+  "mongodb+srv://pedibus:Ped45fimi@cluster0.zfzxyjk.mongodb.net/pedibus?retryWrites=true&w=majority",
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
 
-export default async (req, res) => {
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { latitude, longitude } = req.body;
-    const data = JSON.stringify({ latitude, longitude });
-    fs.writeFileSync(path.resolve("./", "data.json"), data);
-    res.status(200).json({ message: "Position received" });
+
+    try {
+      if (!client.topology || !client.topology.isConnected()) {
+        await client.connect();
+      }
+      const db = client.db("pedibus"); // replace with your database name
+      const collection = db.collection("positions"); // replace with your collection name
+
+      //await collection.insertOne({ latitude, longitude });
+      // Define a constant _id value to always update the same document
+      const documentId = "6537ff14064d2b65bd701ca4";
+      await collection.updateOne(
+        { _id: documentId },
+        {
+          $set: { latitude, longitude },
+        },
+        { upsert: true } // This option creates a new document when no document matches the query criteria.
+      );
+
+      res.status(200).json({ message: "Position updated" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
   } else {
-    res.status(405).send("Method Not Allowed");
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 };
+
+export default handler;
